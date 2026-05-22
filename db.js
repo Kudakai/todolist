9e6c829592e867d563a9bb535884eeb961e47c19
Kudakai <rustamgafarov493@gmail.com>
2026-05-22 16:18:03 +0400

    Update database connection host to use 127.0.0.1

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "app_read_write",
   password: "Theansweris43",
   database: "my_app"
